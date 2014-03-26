fbc6f0418cc8df47fd2d1255e9b5b115c999440c
Wayne Foley <wayne.foley@hp.com>
2014-03-26 10:15:06 -0700

    Adding version info as a linked file in each project, and updating version to 0.9
    Change-Id: Ie839944fa329786d7e7b212b6f85386cba729931

--- Openstack/Openstack.Common/Properties/AssemblyInfo.cs
+++ Openstack/Openstack.Common/Properties/AssemblyInfo.cs
@@ -38,16 +38,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0ed0b3bc-5dcd-488c-a9bd-0940e43afc4a")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("Openstack.Test")]
--- Openstack/Openstack/Properties/AssemblyInfo.cs
+++ Openstack/Openstack/Properties/AssemblyInfo.cs
@@ -38,16 +38,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3a65db89-60cd-49c4-b736-118dce9487ec")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("Openstack.Test")]
